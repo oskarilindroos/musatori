6750381515baec77351b07ba88378ee7a8b3a003
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-04-30 15:49:02 +0300

    added migration function that runs all migrations

---
+++ backend/src/db/migrator.ts
@@ -0,0 +1,36 @@
+import * as path from "path";
+import { fileURLToPath } from "url";
+import { db } from "./db.ts";
+import { promises as fs } from "fs";
+import { Migrator, FileMigrationProvider } from "kysely";
+
+// NOTE: Get __dirname this way because of ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export async function migrateToLatest() {
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      migrationFolder: path.join(__dirname, "migrations"),
+    }),
+  });
+
+  const { error, results } = await migrator.migrateToLatest();
+
+  results?.forEach((it) => {
+    if (it.status === "Success") {
+      console.log(`migration "${it.migrationName}" was executed successfully`);
+    } else if (it.status === "Error") {
+      console.error(`failed to execute migration "${it.migrationName}"`);
+    }
+  });
+
+  if (error) {
+    console.error("failed to migrate");
+    console.error(error);
+    process.exit(1);
+  }
+}
