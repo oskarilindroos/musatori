b21348b3b3a948739c83af188772c357c859c931
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-01 15:19:11 +0300

    removed testing endpoints

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,18 +18,6 @@ app.get("/api/health", (_, res) => {
   res.json({ status: "ok" });
 });
 
-app.get("/api/users", async (_, res) => {
-  const users = await db.selectFrom("users").selectAll().execute();
-  res.json(users);
-});
-
-app.get("/api/listings/categories", async (_, res) => {
-  const categories = await db
-    .selectFrom("listings_categories")
-    .selectAll()
-    .execute();
-  res.json(categories);
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
