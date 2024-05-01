78092cbb1130989869bfee0db6e21afe7bd49775
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-01 23:51:14 +0300

    changed so that the db path is read from env

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -3,7 +3,7 @@ import SQLite from "better-sqlite3";
 import { Kysely, SqliteDialect } from "kysely";
 
 const dialect = new SqliteDialect({
-  database: new SQLite("musatori_db.sqlite"),
+  database: new SQLite(process.env.DB_PATH),
 });
 
 export const db = new Kysely<Database>({
