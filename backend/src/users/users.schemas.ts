7d91d24e9255c9cce156dd0001011506bde42df2
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-02 14:55:42 +0300

    created validation schemas for users

---
+++ backend/src/users/users.schemas.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  username: z.string().min(3),
+  password: z.string().min(8),
+});
+
+export const loginSchema = z.object({
+  username: z.string().min(3),
+  password: z.string().min(8),
+});
