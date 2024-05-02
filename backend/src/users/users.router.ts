268cb2d610fe56820bb4a901be59cb2278a6f786
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-02 14:56:00 +0300

    added validation middleware to signup and login routes

--- backend/src/users/users.router.ts
+++ backend/src/users/users.router.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { usersController } from "./users.controller.js";
 import { verifyToken } from "../middleware/auth.js";
+import { loginSchema, signUpSchema } from "./users.schemas.js";
+import { validate } from "../middleware/validate.js";
 
 const usersRouter = Router();
 
-usersRouter.post("/signup", usersController.signup);
-usersRouter.post("/login", usersController.login);
-
+usersRouter.post("/signup", validate(signUpSchema), usersController.signup);
+usersRouter.post("/login", validate(loginSchema), usersController.login);
 usersRouter.get("/", verifyToken, usersController.getAllUsers);
 usersRouter.get("/:userId", verifyToken, usersController.getUserById);
 usersRouter.patch("/:userId", verifyToken, usersController.updateUser);
