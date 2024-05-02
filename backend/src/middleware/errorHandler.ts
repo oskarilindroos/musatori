89ba5707120ff407e3d64e9d56ba41b862895930
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-02 14:56:15 +0300

    added handling of validation errors to errorhandler

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { ApiError } from "../errors/ApiError.js";
+import { ZodError } from "zod";
 
 export const errorHandler = (
   err: Error,
@@ -7,10 +8,19 @@ export const errorHandler = (
   res: Response,
   _next: NextFunction,
 ) => {
+  // Custom errors
   if (err instanceof ApiError) {
     return res.status(err.statusCode).json({ message: err.message });
   }
 
+  // Validation errors
+  if (err instanceof ZodError) {
+    return res
+      .status(400)
+      .json({ message: "Validation error", validationErrors: err.errors });
+  }
+
+  // All other errors
   console.error(err);
   res.status(500).json({ message: "Internal server error" });
 };
