4fcefe3b0cfeca662ec9b065147eba492a40412b
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-02 10:16:35 +0300

    renamed auth to verifyToken

--- backend/src/users/users.router.ts
+++ backend/src/users/users.router.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
 import { usersController } from "./users.controller.js";
-import { auth } from "../middleware/auth.js";
+import { verifyToken } from "../middleware/auth.js";
 
 const usersRouter = Router();
 
 usersRouter.post("/signup", usersController.signup);
 usersRouter.post("/login", usersController.login);
 
-usersRouter.use(auth);
-usersRouter.get("/", usersController.getAllUsers);
-usersRouter.get("/:userId", usersController.getUserById);
-usersRouter.patch("/:userId", usersController.updateUser);
+usersRouter.get("/", verifyToken, usersController.getAllUsers);
+usersRouter.get("/:userId", verifyToken, usersController.getUserById);
+usersRouter.patch("/:userId", verifyToken, usersController.updateUser);
 
 export { usersRouter };
