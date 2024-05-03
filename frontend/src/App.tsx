22aececfd1362dd3f7ed3898c783e1b86b162ebf
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-03 13:28:42 +0300

    added checking of api health to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { checkHealth } from "./services/checkHealth";
+
 const App = () => {
-  return <h1>Musatori</h1>;
+  const { isPending, isError, error } = useQuery({
+    queryKey: ["apiHealth"],
+    queryFn: checkHealth,
+  });
+
+  if (isPending) return <h1>Connecting to the server...</h1>;
+
+  if (isError) return <h1>Error: {error.message}</h1>;
+
+  return (
+    <div>
+      <h1>Musatori</h1>
+    </div>
+  );
 };
 
 export default App;
