ee1e682d629011a526adf61255fcae527f9efa14
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-03 13:28:25 +0300

    declared axios defaults, and queryclient

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import axios from "axios";
+
+// React-query setup
+const queryClient = new QueryClient();
+
+// Define axios defaults
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
