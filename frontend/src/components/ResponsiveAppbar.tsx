9cd4c74efd17da6fa801a3d8ed1d4c2cb911249c
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-05 00:14:14 +0300

    updated appbar to work with auth

--- frontend/src/components/ResponsiveAppbar.tsx
+++ frontend/src/components/ResponsiveAppbar.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -7,160 +6,107 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import { PianoOutlined } from "@mui/icons-material";
+import { useState, MouseEvent, useContext, Fragment } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
-const settings = ["Account", "My listings", "Logout"];
+const ResponsiveAppBar = () => {
+  const userContext = useContext(UserContext);
+  const navigate = useNavigate();
 
-function ResponsiveAppBar() {
-  // const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-  //   null,
-  // );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null,
-  );
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
 
-  // const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-  //   setAnchorElNav(event.currentTarget);
-  // };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
+  const handleOpenUserMenu = (event: MouseEvent<HTMLElement>) => {
+    // If the user is not logged in, redirect to the auth page
+    if (!userContext.user.isLoggedIn) {
+      navigate("/auth");
+    } else {
+      setAnchorElUser(event.currentTarget);
+    }
   };
 
-  // const handleCloseNavMenu = () => {
-  //   setAnchorElNav(null);
-  // };
+  const handleLogout = () => {
+    handleCloseUserMenu();
+
+    // Log out the user in the context
+    userContext.logout();
+
+    toast.success("Logged out successfully.");
+  };
+
+  const handleNavigateToMyListings = () => {
+    handleCloseUserMenu();
+
+    // Redirect to the user's listings page
+    navigate(`/users/${userContext.user.userId}/listings`);
+  };
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
 
   return (
-    <AppBar color="secondary" position="sticky">
-      <Container maxWidth="lg">
-        <Toolbar disableGutters>
-          <Box display="flex" alignItems="center" flexGrow={1}>
-            <PianoOutlined fontSize="large" />
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              padding={2}
-              sx={{
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textTransform: "uppercase",
-                textDecoration: "none",
-              }}
-            >
-              Musatori
-            </Typography>
-          </Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
+    <Fragment>
+      <AppBar color="secondary" position="sticky">
+        <Container maxWidth="lg">
+          <Toolbar disableGutters>
+            <Box display="flex" alignItems="center" flexGrow={1}>
+              <PianoOutlined fontSize="large" />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                padding={2}
+                sx={{
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textTransform: "uppercase",
+                  textDecoration: "none",
+                }}
+              >
+                Musatori
+              </Typography>
+            </Box>
+            <Box sx={{ flexGrow: 0 }}>
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar
                   sx={{ border: 2, borderColor: "black" }}
                   alt="User avatar"
                 />
               </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <MenuItem onClick={handleNavigateToMyListings}>
+                  <Typography textAlign="center">My listings</Typography>
                 </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+                <MenuItem onClick={handleLogout}>
+                  <Typography textAlign="center">Logout</Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Fragment>
   );
-}
-export default ResponsiveAppBar;
+};
 
-// <Box sx={{ flexGrow: 1 }}>
-//   <IconButton onClick={handleCloseNavMenu}>
-//     <AddCircleOutline
-//       sx={{
-//         color: "black",
-//         fontSize: 50,
-//       }}
-//     />
-//   </IconButton>
-// </Box>
-// <Box sx={{ flexGrow: 1 }}>
-//   <IconButton onClick={handleCloseNavMenu}>
-//     <AddCircleOutline
-//       sx={{
-//         color: "black",
-//         fontSize: 50,
-//       }}
-//     />
-//   </IconButton>
-// </Box>
-// <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-//   {pages.map((page) => (
-//     <Button
-//       key={page}
-//       onClick={handleCloseNavMenu}
-//       sx={{ my: 2, color: "black", display: "block" }}
-//     >
-//       {page}
-//     </Button>
-//   ))}
-// </Box>
-// <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-//   <IconButton
-//     size="large"
-//     aria-label="account of current user"
-//     aria-controls="menu-appbar"
-//     aria-haspopup="true"
-//     onClick={handleOpenNavMenu}
-//     color="inherit"
-//   >
-//     <MenuIcon />
-//   </IconButton>
-//   <Menu
-//     id="menu-appbar"
-//     anchorEl={anchorElNav}
-//     anchorOrigin={{
-//       vertical: "bottom",
-//       horizontal: "left",
-//     }}
-//     keepMounted
-//     transformOrigin={{
-//       vertical: "top",
-//       horizontal: "left",
-//     }}
-//     open={Boolean(anchorElNav)}
-//     onClose={handleCloseNavMenu}
-//     sx={{
-//       display: { xs: "block", md: "none" },
-//     }}
-//   >
-//     {pages.map((page) => (
-//       <MenuItem key={page} onClick={handleCloseNavMenu}>
-//         <Typography textAlign="center">{page}</Typography>
-//       </MenuItem>
-//     ))}
-//   </Menu>
-// </Box>
+export default ResponsiveAppBar;
