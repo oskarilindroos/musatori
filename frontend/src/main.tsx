50bce400e1181de7027077021f3d0f4905434a9d
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-04 12:15:06 +0300

    organized code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import axios from "axios";
 
 // React-query setup
 const queryClient = new QueryClient();
