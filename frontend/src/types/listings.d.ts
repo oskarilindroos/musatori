fb5499156b9cabeac77159dcc85eefbc19bbb92c
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-05 13:44:25 +0300

    added type for a new listing

--- frontend/src/types/listings.d.ts
+++ frontend/src/types/listings.d.ts
@@ -12,6 +12,16 @@ export interface Listing {
   image_url: string;
 }
 
+export interface NewListing {
+  title: string;
+  description: string;
+  location: string;
+  price: number | null;
+  listing_type_id: number;
+  listing_category_id: number;
+  imageUrls: string[];
+}
+
 export interface ListingDetails {
   id: number;
   title: string;
