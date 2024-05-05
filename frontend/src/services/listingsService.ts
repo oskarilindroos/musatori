eda5f443463cc6e96bae8bdf91d9ef398ea994e2
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-05 16:46:49 +0300

    added more methods to listings service

--- frontend/src/services/listingsService.ts
+++ frontend/src/services/listingsService.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
-import { Listing, ListingDetails, NewListing } from "../types/listings";
+import {
+  Listing,
+  ListingCategory,
+  ListingDetails,
+  ListingType,
+  NewListing,
+} from "../types/listings";
 
 const getAllListings = async () => {
   return (await axios.get<Listing[]>("/api/listings")).data;
@@ -9,18 +15,29 @@ const getListingById = async (id: string) => {
   return (await axios.get<ListingDetails>(`/api/listings/${id}`)).data;
 };
 
-const postListing = async (listing: NewListing, token: string) => {
+const postListing = async (listing: NewListing) => {
+  console.log(listing);
   return (
     await axios.post<Listing>("/api/listings", listing, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${listing.token}`,
       },
     })
   ).data;
 };
 
+const getAllCategories = async () => {
+  return (await axios.get<ListingCategory[]>("/api/listings/categories")).data;
+};
+
+const getAllTypes = async () => {
+  return (await axios.get<ListingType[]>("/api/listings/types")).data;
+};
+
 export const listingsService = {
   getAllListings,
   getListingById,
+  getAllCategories,
+  getAllTypes,
   postListing,
 };
