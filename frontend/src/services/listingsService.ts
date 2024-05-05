7971c1b2b1adcee41966c5411d69ab9da5e125ec
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-05 13:44:14 +0300

    added function for posting a new listing to the api

--- frontend/src/services/listingsService.ts
+++ frontend/src/services/listingsService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Listing, ListingDetails } from "../types/listings";
+import { Listing, ListingDetails, NewListing } from "../types/listings";
 
 const getAllListings = async () => {
   return (await axios.get<Listing[]>("/api/listings")).data;
@@ -9,7 +9,18 @@ const getListingById = async (id: string) => {
   return (await axios.get<ListingDetails>(`/api/listings/${id}`)).data;
 };
 
+const postListing = async (listing: NewListing, token: string) => {
+  return (
+    await axios.post<Listing>("/api/listings", listing, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+  ).data;
+};
+
 export const listingsService = {
   getAllListings,
   getListingById,
+  postListing,
 };
